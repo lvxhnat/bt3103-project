b38c293f182f4ffb4e586f21cca33483b35d3712
tinghwei <e0968057@u.nus.edu>
2024-03-24 18:01:44 +0800

    Updated BusinessLogin page

--- src/views/Auth/Login/BusinessLoginPage.vue
+++ src/views/Auth/Login/BusinessLoginPage.vue
@@ -1,48 +1,82 @@
 <template>
-  <div class="login-container">
-    <NavBar />
-    <div class="login-form">
-      <div class="left-panel">
-        <img src="../../../assets/auth/business_login.png" />
-      </div>
-      <div class="right-panel">
-        <div class="login-page-wrapper">
-          <img src="../../../assets/auth/business_topimg.png" width="100px" />
-          <h1 class="login-title">BUSINESS LOGIN</h1>
-          <div class="input-wrapper">
-            <form @submit.prevent="login">
-              <div class="input-group">
-                <label class="label">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  v-model="business.email"
-                  placeholder="Enter Email"
-                  required
-                />
-              </div>
-              <div class="input-group">
-                <label class="label">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  v-model="business.password"
-                  placeholder="Enter Password"
-                  required
-                />
-              </div>
-              <div class="form-button">
-                <button type="submit" class="login-button">LOGIN</button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div class="signup-link">
-          Don’t have an account? <a href="/signup">Sign up now!</a>
-        </div>
-      </div>
+  <NavBar />
+  <v-app>
+    <div class="login-container">
+      <v-content>
+        <v-container fluid class="pa-16 ma-5">
+          <v-row>
+            <v-col cols="6" class="d-flex justify-center">
+              <img
+                src="../../../assets/auth/business_login.png"
+                height="450"
+                width="419.25"
+              />
+            </v-col>
+            <v-col cols="6" class="d-flex justify-center">
+              <v-form @submit.prevent="login">
+                <v-card
+                  class="mx-auto pa-12 pb-8"
+                  elevation="8"
+                  width="400"
+                  height="450"
+                  rounded="lg"
+                >
+                  <div class="text-h6 mb-6">Business Login</div>
+                  <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+                  <v-text-field
+                    v-model="business.email"
+                    density="compact"
+                    placeholder="Email Address"
+                    prepend-inner-icon="mdi-email-outline"
+                    variant="outlined"
+                  ></v-text-field>
+                  <div
+                    class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+                  >
+                    Password
+                    <a
+                      class="text-caption text-decoration-none text-green"
+                      href="#"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      >Forgot password?</a
+                    >
+                  </div>
+                  <v-text-field
+                    v-model="business.password"
+                    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible ? 'text' : 'password'"
+                    density="compact"
+                    placeholder="Enter your password"
+                    prepend-inner-icon="mdi-lock-outline"
+                    variant="outlined"
+                    @click:append-inner="visible = !visible"
+                  ></v-text-field>
+                  <v-btn
+                    block
+                    class="mb-8"
+                    color="#118951"
+                    size="large"
+                    variant="tonal"
+                    >Log In</v-btn
+                  >
+                  <v-card-text class="text-center">
+                    <a
+                      class="text-green text-decoration-none"
+                      href="/register/business"
+                      rel="noopener noreferrer"
+                      >Sign up now
+                      <v-icon icon="mdi-chevron-right"></v-icon>
+                    </a>
+                  </v-card-text>
+                </v-card>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -69,9 +103,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.right-panel {
-  padding-left: 100px;
-}
-</style>
