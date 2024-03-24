83018c1888880ad1129ec7efcb686cb4a7f4ba69
Loh Yi Kuang <yikuang5@gmail.com>
2024-03-24 14:03:52 +0800

    edit: added AuthInputBox component

---
+++ src/components/AuthInputBox/AuthInputBox.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="login-page-wrapper">
+    <img :src="getImageURL" width="100px" />
+    <h1 class="login-title">{{ title }}</h1>
+    <div class="input-wrapper">
+      <form @submit.prevent="login">
+        <div class="input-group">
+          <label class="label">Email</label>
+          <input
+            type="email"
+            id="email"
+            v-model="client.email"
+            placeholder="Enter Email"
+            required
+          />
+        </div>
+        <div class="input-group">
+          <label class="label">Password</label>
+          <input
+            type="password"
+            id="password"
+            v-model="client.password"
+            placeholder="Enter Password"
+            required
+          />
+        </div>
+        <div class="form-button">
+          <button type="submit" class="login-button">LOGIN</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import styles from './style.css'
+export default {
+  name: 'AuthInputBox',
+  props: {
+    title: String,
+    imageURL: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      client: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  computed: {
+    getImageURL() {
+      return require(`@/assets/auth/${this.imageURL}`)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- src/views/Auth/Login/BusinessLoginPage.vue
+++ src/views/Auth/Login/BusinessLoginPage.vue
@@ -6,37 +6,7 @@
         <img src="../../../assets/auth/business_login.png" />
       </div>
       <div class="right-panel">
-        <div class="login-page-wrapper">
-          <img src="../../../assets/auth/business_topimg.png" width="100px" />
-          <h1 class="login-title">BUSINESS LOGIN</h1>
-          <div class="input-wrapper">
-            <form @submit.prevent="login">
-              <div class="input-group">
-                <label class="label">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  v-model="business.email"
-                  placeholder="Enter Email"
-                  required
-                />
-              </div>
-              <div class="input-group">
-                <label class="label">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  v-model="business.password"
-                  placeholder="Enter Password"
-                  required
-                />
-              </div>
-              <div class="form-button">
-                <button type="submit" class="login-button">LOGIN</button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <AuthInputBox imageURL="globe.png" title="BUSINESS LOGIN" />
         <div class="signup-link">
           Don’t have an account? <a href="/signup">Sign up now!</a>
         </div>
@@ -47,6 +17,7 @@
 
 <script>
 import NavBar from '@/components/NavBar'
+import AuthInputBox from '@/components/AuthInputBox'
 import styles from './style.css'
 
 export default {
@@ -66,6 +37,7 @@ export default {
   },
   components: {
     NavBar,
+    AuthInputBox,
   },
 }
 </script>
