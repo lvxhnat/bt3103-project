db820792e072b14ab08f3f737cbf6e79f5affe82
tinghwei <e0968057@u.nus.edu>
2024-04-08 00:24:15 +0800

    Updated NavBar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -3,34 +3,49 @@
     <v-app-bar
       style="padding: 0px 90px"
       app
-      color="#F8F4ED"
+      color="#118951"
       dark
       elevation="6"
       elevate-on-scroll
     >
-      <v-app-bar-title style="color: #118951">SECONDSERVINGS</v-app-bar-title>
+      <router-link to="/" style="text-decoration: none; color: inherit">
+        <v-app-bar-title style="color: #f8f4ed">SECONDSERVINGS</v-app-bar-title>
+      </router-link>
 
       <v-spacer></v-spacer>
 
-      <v-list class="d-flex align center" style="background-color: #f8f4ed">
-        <!-- <v-list-item
-          link
-          rounded
-          v-for="menu in menus"
-          :key="menu.title"
-          :to="menu.route"
-          style="background-color: #f8f4ed"
-        >
-          <v-list-item-title style="color: black"
-            >{{ menu.title }}
-          </v-list-item-title>
-        </v-list-item> -->
-        <v-btn variants="outlined" color="#118951" @click="navigateToUserLogin">
-          USER LOGIN
-        </v-btn>
-        <v-btn outlined color="#118951" @click="navigateToBusinessLogin">
-          BUSINESS LOGIN
-        </v-btn>
+      <v-list class="d-flex align center" style="background-color: #118951">
+        <template v-if="!user">
+          <v-btn
+            variants="outlined"
+            color="#f8f4ed"
+            @click="navigateToUserLogin"
+          >
+            USER LOGIN
+          </v-btn>
+          <v-btn outlined color="#f8f4ed" @click="navigateToBusinessLogin">
+            BUSINESS LOGIN
+          </v-btn>
+        </template>
+        <template v-else>
+          <!-- Display "USER PROFILE" button for user accounts -->
+          <template v-if="user.accountType === 'user'">
+            <v-btn
+              variants="outlined"
+              color="#f8f4ed"
+              @click="navigateToUserProfile"
+            >
+              USER PROFILE
+            </v-btn>
+          </template>
+          <!-- Display "BUSINESS PROFILE" button for business accounts -->
+          <template v-else-if="user.accountType === 'business'">
+            <v-btn outlined color="#f8f4ed" @click="navigateToBusinessProfile">
+              BUSINESS PROFILE
+            </v-btn>
+          </template>
+          <v-btn outlined color="#f8f4ed" @click="logout"> LOGOUT </v-btn>
+        </template>
       </v-list>
     </v-app-bar>
   </v-app>
@@ -38,19 +53,14 @@
 
 <script>
 import styles from './style.css'
-import { defineComponent, computed } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
+import { auth, db } from '../../firebaseConfig'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 
 export default defineComponent({
   name: 'NavBar',
-  /*data() {
-    return {
-      menus: [
-        { title: 'Home', route: '/' },
-        { title: 'About Us', route: '/About/Us' },
-      ],
-    }
-  },*/
   setup() {
     const router = useRouter()
     const route = useRoute()
@@ -62,32 +72,68 @@ export default defineComponent({
       router.push('/login/business')
     }
 
-    const navigateHome = () => {
-      router.push('/')
+    const navigateToUserProfile = () => {
+      router.push('/profile/user')
+    }
+    const navigateToBusinessProfile = () => {
+      router.push('/profile/business')
     }
 
-    // Computed properties for image sources
-    const logoImage = computed(() => {
-      return route.path === '/profile/user' ||
-        route.path === '/profile/business'
-        ? require('../../assets/logo/profile.png')
-        : require('../../assets/logo/logo.png')
-    })
-    const logoSideImage = computed(() => {
-      // Assuming you want to change both images, otherwise adjust as needed
-      return route.path === '/profile/user' ||
-        route.path === '/profile/business'
-        ? require('../../assets/logo/profile.png')
-        : require('../../assets/logo/logo-side.png')
+    const user = ref(null)
+
+    const logout = async () => {
+      try {
+        await auth.signOut()
+        alert('Signed out successfully.')
+        router.push('/')
+      } catch (error) {
+        console.error('Error logging out:', error.message)
+        alert('Error logging out:', error.message)
+      }
+    }
+
+    onAuthStateChanged(auth, async (newUser) => {
+      if (newUser) {
+        try {
+          let userData = null
+          const userSnapshot = await getDocs(
+            query(collection(db, 'users'), where('userId', '==', newUser.uid))
+          )
+          if (!userSnapshot.empty) {
+            userData = userSnapshot.docs[0].data()
+          } else {
+            const businessSnapshot = await getDocs(
+              query(
+                collection(db, 'businesses'),
+                where('userId', '==', newUser.uid)
+              )
+            )
+            if (!businessSnapshot.empty) {
+              userData = businessSnapshot.docs[0].data()
+            }
+          }
+          if (userData) {
+            user.value = {
+              ...userData,
+              uid: newUser.uid,
+            }
+          }
+        } catch (error) {
+          console.error('Error fetching user data:', error.message)
+          // Handle error fetching user data
+        }
+      } else {
+        user.value = null
+      }
     })
 
     return {
-      navigateHome,
+      user,
       navigateToUserLogin,
       navigateToBusinessLogin,
-      logoImage,
-      logoSideImage,
-      route,
+      navigateToUserProfile,
+      navigateToBusinessProfile,
+      logout,
     }
   },
 })
