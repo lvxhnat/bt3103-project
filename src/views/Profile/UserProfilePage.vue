c3646318f522821db1264be10f76185257f5cb13
tinghwei <e0968057@u.nus.edu>
2024-04-08 18:47:56 +0800

    Updated Profile pages

--- src/views/Profile/UserProfilePage.vue
+++ src/views/Profile/UserProfilePage.vue
@@ -1,11 +1,11 @@
 <template>
   <NavBar />
   <v-app>
-    <div class="landing-container">
+    <div class="main-container">
       <v-content>
-        <v-container fluid class="pa-3 ma-2">
-          <div class="main-container">
-            <div class="left-panel">
+        <v-container fluid class="pa-5 ma-2">
+          <v-row>
+            <v-col cols="6" class="d-flex justify-center">
               <div class="account-details">
                 <h1 class="account-details-title">Account Details</h1>
                 <div class="input-container">
@@ -25,66 +25,68 @@
                   <input class="input-acc" />
                 </div>
               </div>
-            </div>
+            </v-col>
 
-            <div class="right-panel">
-              <div class="current-listings-container">
-                <h1 class="account-details-title">Current Listings</h1>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Item</th>
-                      <th>Quantity</th>
-                      <th>Options</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="item in items" :key="item.id">
-                      <td>
-                        <div>{{ item.name }}</div>
-                        <div>(ID {{ item.id }})</div>
-                      </td>
-                      <td class="cell-increment">
-                        <span>{{ item.quantity }}</span>
-                        <button
-                          class="increment-button"
-                          @click="increment(item)"
-                        >
-                          +
-                        </button>
-                        <button
-                          class="increment-button"
-                          @click="decrement(item)"
-                        >
-                          -
-                        </button>
-                      </td>
-                      <td>
-                        <button
-                          class="remove-button"
-                          @click="removeItem(item.id)"
-                        >
-                          Remove Item
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div class="table-bottom">
-                  <button class="add-button">Add Items</button>
-                  <button class="add-button">Checkout</button>
+            <v-col cols="6" class="d-flex justify-center">
+              <v-row class="right-container">
+                <div class="current-listings-container">
+                  <h1 class="account-details-title">Current Listings</h1>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Options</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="item in items" :key="item.id">
+                        <td>
+                          <div>{{ item.name }}</div>
+                          <div>(ID {{ item.id }})</div>
+                        </td>
+                        <td class="cell-increment">
+                          <span>{{ item.quantity }}</span>
+                          <button
+                            class="increment-button"
+                            @click="increment(item)"
+                          >
+                            +
+                          </button>
+                          <button
+                            class="increment-button"
+                            @click="decrement(item)"
+                          >
+                            -
+                          </button>
+                        </td>
+                        <td>
+                          <button
+                            class="remove-button"
+                            @click="removeItem(item.id)"
+                          >
+                            Remove Item
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div class="table-bottom">
+                    <button class="add-button">Add Items</button>
+                    <button class="add-button">Checkout</button>
+                  </div>
                 </div>
-              </div>
-              <div class="wallet">
-                <h1 class="account-details-title">Wallet</h1>
-                <div class="balance-container">
-                  <h3 class="balance-side">Balance:</h3>
-                  <input class="input-acc" />
-                  <button class="top-up-button">Top-up</button>
+                <div class="wallet">
+                  <h1 class="account-details-title">Wallet</h1>
+                  <div class="balance-container">
+                    <h3 class="balance-side">Balance:</h3>
+                    <input class="input-acc" />
+                    <button class="top-up-button">Top-up</button>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
+              </v-row>
+            </v-col>
+          </v-row>
         </v-container>
       </v-content>
     </div>
