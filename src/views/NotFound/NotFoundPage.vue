08a8f2a7a3921480fb6290112cfe57551a22259e
Yang Mingrui <e0958440@u.nus.edu>
2024-04-20 11:01:59 +0800

    slight changes to frontend of notfound page

--- src/views/NotFound/NotFoundPage.vue
+++ src/views/NotFound/NotFoundPage.vue
@@ -3,7 +3,9 @@
     <v-app>
         <div class="main-container">
             <v-content>
-                <v-container fluid class="pa-5 ma-2">
+                <v-container fluid class="pa-16 ma-5">
+                    <img src="../../assets/auth/registration_floret_1.png" class="img-flore-1" />
+                    <img src="../../assets/auth/registration_floret_2.png" class="img-flore-2" />
                     <v-row>
                         <v-col cols="12" class="d-flex justify-center">
                             <img src="../../assets/notfound/pagenotfound.png" height="450" />
