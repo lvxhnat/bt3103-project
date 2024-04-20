fee28426064ed3fed7ed161b4ee9e153933c861d
Yang Mingrui <e0958440@u.nus.edu>
2024-04-21 00:11:58 +0800

    add v-for to catalogue page

--- src/views/Catalogue/CataloguePage.vue
+++ src/views/Catalogue/CataloguePage.vue
@@ -1,88 +1,48 @@
 <template>
-    <NavBar />
-    <div class="landing-container">
+  <NavBar />
+  <div class="landing-container">
     <div class="choose-wrapper">
-        <div class = "header">
-            <h1>Choose from your favourite stores</h1>
-        </div>
-        
-        <div class = "establishments">
+      <div class="header">
+        <h1>Choose from your favourite stores</h1>
+      </div>
+
+      <div class="establishments">
         <div class="establishments-wrapper">
-        <Establishment
-          :numStars="4"
-          title="The Baker's Dozen"
-          place="Raffles Place"
-          imageURL="bakers-dozen.png"
-        />
-
-        <Establishment
-          :numStars="5"
-          title="GreenMart"
-          place="Clementi Mall"
-          imageURL="greenmart.png"
-        />
-
-        <Establishment
-          :numStars="3"
-          title="Mimi's Pizzeria"
-          place="Jurong Point"
-          imageURL="mimi-pizzeria.png"
-        />
-
-        <Establishment
-          :numStars="5"
-          title="SweetShakes"
-          place="Clementi Mall"
-          imageURL="sweetshakes.png"
-        />
-
-        <Establishment
-          :numStars="4"
-          title="The Big Stack"
-          place="Westgate"
-          imageURL="the-big-stack.png"
-        />
-
-        <Establishment
-          :numStars="4"
-          title="Filly's Taco"
-          place="City Square Mall"
-          imageURL="fillys-taco.png"
-        />
-
-        <Establishment
-          :numStars="5"
-          title="The Fruit Stop"
-          place="NEX Mall"
-          imageURL="the-fruit-stop.png"
-        />
-
-        <Establishment
-          :numStars="5"
-          title="JP Bakery"
-          place="Jurong Point"
-          imageURL="jp-bakery.png"
-        />
+          <Establishment v-for="business in businesses" :key="business.title" :numStars="business.numStars"
+            :title="business.title" :place="business.place" :imageURL="business.imageURL" />
         </div>
       </div>
     </div>
   </div>
 </template>
-  
+
 <script>
 import NavBar from '@/components/NavBar'
 import Establishment from '@/components/Establishment'
-  
+
 export default {
-    name: 'CataloguePage',
-    components: {
-        NavBar,
-        Establishment,
-    },
+  name: 'CataloguePage',
+  components: {
+    NavBar,
+    Establishment,
+  },
+  data() {
+    return {
+      businesses: [
+        { numStars: '4', title: "The Baker's Dozen", place: 'Raffles Place', imageURL: 'bakers-dozen.png' },
+        { numStars: '5', title: "GreenMart", place: 'Clementi Mall', imageURL: 'greenmart.png' },
+        { numStars: '3', title: "Mimi's Pizzeria", place: 'Jurong Point', imageURL: 'mimi-pizzeria.png' },
+        { numStars: '5', title: "SweetShakes", place: 'Clementi Mall', imageURL: 'sweetshakes.png' },
+        { numStars: '4', title: "The Big Stack", place: 'Westgate', imageURL: 'the-big-stack.png' },
+        { numStars: '4', title: "Filly's Taco", place: 'City Square Mall', imageURL: 'fillys-taco.png' },
+        { numStars: '5', title: "The Fruit Stop", place: 'NEX Mall', imageURL: 'the-fruit-stop.png' },
+        { numStars: '5', title: "JP Bakery", place: 'Jurong Point', imageURL: 'jp-bakery.png' },
+      ]
+    }
+  }
 }
 </script>
-  
+
 <style scoped>
 @import './style.css';
 </style>
-  
