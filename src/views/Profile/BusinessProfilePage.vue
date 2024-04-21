0741bf55a3a9d8238031fd1f187a8e14edbc732e
tinghwei <e0968057@u.nus.edu>
2024-04-21 17:56:56 +0800

    Updated Business and User profile page

--- src/views/Profile/BusinessProfilePage.vue
+++ src/views/Profile/BusinessProfilePage.vue
@@ -1,82 +1,77 @@
 <template>
   <NavBar />
   <div class="main-container">
-    <v-container fluid class="pa-4 ma-3">
+    <v-container fluid class="pt-6 ma-2">
       <v-row>
         <v-col cols="6" class="d-flex justify-center">
-          <div class="account-details-container">
-            <BusinessAccountDetails />
-          </div>
+          <BusinessAccountDetails />
         </v-col>
         <v-col cols="6" class="d-flex justify-center">
           <v-row class="right-container">
-            <div class="current-listings-container">
-              <div class="text-h6 mb-4">Current Listings</div>
-              <div class="table-container">
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Item</th>
-                      <th>Quantity</th>
-                      <th>Options</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="item in items" :key="item.id">
-                      <td>
-                        <div>{{ item.name }}</div>
-                        <div>(ID {{ item.id }})</div>
-                      </td>
-                      <td class="cell-increment">
-                        <span>{{ item.quantity }}</span>
-                        <button
-                          class="increment-button"
-                          @click="increment(item)"
-                        >
-                          +
-                        </button>
-                        <button
-                          class="increment-button"
-                          @click="decrement(item)"
-                        >
-                          -
-                        </button>
-                      </td>
-                      <td>
-                        <button
-                          class="remove-button"
-                          @click="removeItem(item.id)"
-                        >
-                          Remove Item
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div class="table-bottom">
-                <button class="add-button" @click="navitoAddItems">
-                  Add Items
-                </button>
-                <button class="add-button">Checkout</button>
-              </div>
-            </div>
-            <div class="wallet">
-              <div class="text-h6 mb-4">Wallet</div>
-              <div class="balance-container">
-                <div class="text-subtitle-1 text-medium-emphasis">Balance</div>
-                <v-text-field
-                  placeholder="No data as of now"
-                  density="compact"
-                  prepend-inner-icon="mdi-account-cash-outline"
-                  variant="outlined"
-                  readonly
-                ></v-text-field>
-                <button class="top-up-button" @click="naviToWallet">
-                  Top-up
-                </button>
-              </div>
-            </div>
+            <v-card class="mb-4">
+              <v-card-title>Current Listings</v-card-title>
+              <v-card-item class="pb-4 pl-4 pr-4">
+                <div class="business-table-container">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th width="30%">Item</th>
+                        <th width="30%">Quantity</th>
+                        <th width="40%">Options</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="item in items" :key="item.id">
+                        <td>
+                          <div>{{ item.name }}</div>
+                          <div>(ID {{ item.id }})</div>
+                        </td>
+                        <td class="cell-increment">
+                          <span>{{ item.quantity }}</span>
+                          <button
+                            class="increment-button"
+                            @click="increment(item)"
+                          >
+                            +
+                          </button>
+                          <button
+                            class="increment-button"
+                            @click="decrement(item)"
+                          >
+                            -
+                          </button>
+                        </td>
+                        <td>
+                          <v-btn
+                            class="remove-button"
+                            @click="removeItem(item.id)"
+                          >
+                            Remove Item
+                          </v-btn>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </v-card-item>
+              <v-card-actions>
+                <v-btn @click="navitoAddItems"> Add Items </v-btn>
+                <v-btn>Checkout</v-btn>
+              </v-card-actions>
+            </v-card>
+
+            <v-card tonal color="#118951">
+              <v-card-title>E-wallet Balance</v-card-title>
+              <v-card-subtitle>Balance</v-card-subtitle>
+              <v-card-item>
+                <div>
+                  <div class="text-h6 mb-1">S${{ balance }}</div>
+                </div>
+              </v-card-item>
+              <v-card-actions>
+                <v-btn @click="naviToWallet">Top-up</v-btn>
+              </v-card-actions>
+            </v-card>
           </v-row>
         </v-col>
       </v-row>
@@ -88,11 +83,16 @@
 import NavBar from '@/components/NavBar'
 import BusinessAccountDetails from '../../components/AccountDetails/BusinessAccountDetails.vue'
 import { useRouter } from 'vue-router'
+import { db } from '@/firebaseConfig'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default {
   name: 'BusinessProfilePage',
   data() {
     return {
+      balance: 0,
+      useremail: '',
       items: [
         { id: 'XXX', name: 'Bread', quantity: 10 },
         { id: 'XYZ', name: 'Potato', quantity: 7 },
@@ -112,6 +112,19 @@ export default {
 
     return { naviToWallet, navitoAddItems }
   },
+  async mounted() {
+    const auth = getAuth()
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        this.useremail = user.email
+        console.log(this.useremail)
+        await this.fetchAndUpdateData(this.useremail)
+      } else {
+        // Redirect to home page
+        this.$router.push('/')
+      }
+    })
+  },
   methods: {
     increment(item) {
       item.quantity++
@@ -123,6 +136,17 @@ export default {
     },
     removeItem(itemId) {
       this.items = this.items.filter((item) => item.id !== itemId)
+    },
+    async fetchAndUpdateData(useremail) {
+      try {
+        const querySnapShot = await getDoc(doc(db, 'Top Up', useremail))
+        const data = querySnapShot.data()
+        this.balance = data.balance
+      } catch (error) {
+        // const errorCode = error.code
+        const errorMessage = error.message
+        alert(errorMessage)
+      }
     },
     navitoAddItems() {
       this.$router.push({ path: '/business/additems' })
--- src/views/Profile/UserProfilePage.vue
+++ src/views/Profile/UserProfilePage.vue
@@ -1,26 +1,23 @@
 <template>
   <NavBar />
-  <v-app>
-    <div class="main-container">
-      <v-container fluid class="pa-5 ma-2">
-        <v-row>
-          <v-col cols="6" class="d-flex justify-center">
-            <div class="account-details-container">
-              <UserAccountDetails />
-            </div>
-          </v-col>
-
-          <v-col cols="6" class="d-flex justify-center">
-            <v-row class="right-container">
-              <div class="current-listings-container">
-                <div class="text-h6 mb-4">My Cart</div>
-                <div class="table-container">
+  <div class="main-container">
+    <v-container fluid class="pt-6 ma-2">
+      <v-row>
+        <v-col cols="6" class="d-flex justify-center">
+          <UserAccountDetails />
+        </v-col>
+        <v-col cols="6" class="d-flex justify-center">
+          <v-row class="right-container">
+            <v-card class="mb-4">
+              <v-card-title>My Cart</v-card-title>
+              <v-card-item class="pb-4 pl-4 pr-4">
+                <div class="user-table-container">
                   <table>
                     <thead>
                       <tr>
-                        <th>Item</th>
-                        <th>Quantity</th>
-                        <th>Options</th>
+                        <th width="30%">Item</th>
+                        <th width="30%">Quantity</th>
+                        <th width="40%">Options</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -45,56 +42,104 @@
                           </button>
                         </td>
                         <td>
-                          <button
+                          <v-btn
                             class="remove-button"
                             @click="removeItem(item.id)"
                           >
                             Remove Item
-                          </button>
+                          </v-btn>
                         </td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
-                <div class="table-bottom">
-                  <button class="add-button">Add Items</button>
-                  <button class="add-button">Checkout</button>
-                </div>
-              </div>
+              </v-card-item>
+              <v-card-actions>
+                <v-btn @click="navitoAddItems"> Add Items </v-btn>
+                <v-btn>Checkout</v-btn>
+              </v-card-actions>
+            </v-card>
 
-              <div class="wallet">
-                <div class="text-h6 mb-4">Wallet</div>
-                <div class="balance-container">
-                  <div class="text-subtitle-1 text-medium-emphasis">
-                    Balance
-                  </div>
-                  <v-text-field
-                    placeholder="No data as of now"
-                    density="compact"
-                    prepend-inner-icon="mdi-account-cash-outline"
-                    variant="outlined"
-                    readonly
-                  ></v-text-field>
-                  <button class="top-up-button" @click="naviToWallet">
-                    Top-up
-                  </button>
+            <v-card tonal color="#118951">
+              <v-card-title>E-wallet Balance</v-card-title>
+              <v-card-subtitle>Balance</v-card-subtitle>
+              <v-card-item>
+                <div>
+                  <div class="text-h6 mb-1">S${{ balance }}</div>
                 </div>
-              </div>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
-  </v-app>
+              </v-card-item>
+              <v-card-actions>
+                <v-btn @click="naviToWallet">Top-up</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar'
 import UserAccountDetails from '../../components/AccountDetails/UserAccountDetails.vue'
 import { useRouter } from 'vue-router'
+import { db } from '@/firebaseConfig'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default {
   name: 'UserProfilePage',
+  data() {
+    return {
+      balance: 0,
+      useremail: '',
+      items: [
+        { id: 'XXX', name: 'Bread', quantity: 10 },
+        { id: 'XYZ', name: 'Potato', quantity: 7 },
+        { id: 'XXX', name: 'Bread', quantity: 10 },
+        { id: 'XYZ', name: 'Potato', quantity: 7 },
+        { id: 'XXX', name: 'Bread', quantity: 10 },
+        { id: 'XYZ', name: 'Potato', quantity: 7 },
+      ],
+    }
+  },
+  async mounted() {
+    const auth = getAuth()
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        this.useremail = user.email
+        console.log(this.useremail)
+        await this.fetchAndUpdateData(this.useremail)
+      } else {
+        // Redirect to home page
+        this.$router.push('/')
+      }
+    })
+  },
+  methods: {
+    increment(item) {
+      item.quantity++
+    },
+    decrement(item) {
+      if (item.quantity > 0) {
+        item.quantity--
+      }
+    },
+    removeItem(itemId) {
+      this.items = this.items.filter((item) => item.id !== itemId)
+    },
+    async fetchAndUpdateData(useremail) {
+      try {
+        const querySnapShot = await getDoc(doc(db, 'Top Up', useremail))
+        const data = querySnapShot.data()
+        this.balance = data.balance
+      } catch (error) {
+        // const errorCode = error.code
+        const errorMessage = error.message
+        alert(errorMessage)
+      }
+    },
+  },
   components: {
     NavBar,
     UserAccountDetails,
