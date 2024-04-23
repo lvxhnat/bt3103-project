f5b6732accd1d713ba27d0a059729796e4cde93d
Yang Mingrui <e0958440@u.nus.edu>
2024-04-23 14:40:43 +0800

    display businesses in database to catalogue and landing pages

--- src/views/Catalogue/CataloguePage.vue
+++ src/views/Catalogue/CataloguePage.vue
@@ -8,14 +8,8 @@
 
       <div class="StoreCards">
         <div class="StoreCards-wrapper">
-          <StoreCard
-            v-for="business in businesses"
-            :key="business.title"
-            :numStars="business.numStars"
-            :title="business.title"
-            :place="business.place"
-            :imageURL="business.imageURL"
-          />
+          <StoreCard v-for="business in businesses" :key="business.title" :numStars="business.numStars"
+            :title="business.title" :place="business.place" :imageURL="business.imageURL" />
         </div>
       </div>
     </div>
@@ -25,6 +19,8 @@
 <script>
 import NavBar from '@/components/NavBar'
 import StoreCard from '@/components/StoreCard'
+import { db } from '@/firebaseConfig'
+import { getDocs, collection } from 'firebase/firestore'
 
 export default {
   name: 'CataloguePage',
@@ -34,58 +30,84 @@ export default {
   },
   data() {
     return {
-      businesses: [
-        {
-          numStars: '4',
-          title: "The Baker's Dozen",
-          place: 'Raffles Place',
-          imageURL: 'bakers-dozen.png',
-        },
-        {
-          numStars: '5',
-          title: 'GreenMart',
-          place: 'Clementi Mall',
-          imageURL: 'greenmart.png',
-        },
-        {
-          numStars: '3',
-          title: "Mimi's Pizzeria",
-          place: 'Jurong Point',
-          imageURL: 'mimi-pizzeria.png',
-        },
-        {
-          numStars: '5',
-          title: 'SweetShakes',
-          place: 'Clementi Mall',
-          imageURL: 'sweetshakes.png',
-        },
-        {
-          numStars: '4',
-          title: 'The Big Stack',
-          place: 'Westgate',
-          imageURL: 'the-big-stack.png',
-        },
-        {
-          numStars: '4',
-          title: "Filly's Taco",
-          place: 'City Square Mall',
-          imageURL: 'fillys-taco.png',
-        },
-        {
-          numStars: '5',
-          title: 'The Fruit Stop',
-          place: 'NEX Mall',
-          imageURL: 'the-fruit-stop.png',
-        },
-        {
-          numStars: '5',
-          title: 'JP Bakery',
-          place: 'Jurong Point',
-          imageURL: 'jp-bakery.png',
-        },
-      ],
+      // businesses: [
+      //   {
+      //     numStars: '4',
+      //     title: "The Baker's Dozen",
+      //     place: 'Raffles Place',
+      //     imageURL: 'bakers-dozen.png',
+      //   },
+      //   {
+      //     numStars: '5',
+      //     title: 'GreenMart',
+      //     place: 'Clementi Mall',
+      //     imageURL: 'greenmart.png',
+      //   },
+      //   {
+      //     numStars: '3',
+      //     title: "Mimi's Pizzeria",
+      //     place: 'Jurong Point',
+      //     imageURL: 'mimi-pizzeria.png',
+      //   },
+      //   {
+      //     numStars: '5',
+      //     title: 'SweetShakes',
+      //     place: 'Clementi Mall',
+      //     imageURL: 'sweetshakes.png',
+      //   },
+      //   {
+      //     numStars: '4',
+      //     title: 'The Big Stack',
+      //     place: 'Westgate',
+      //     imageURL: 'the-big-stack.png',
+      //   },
+      //   {
+      //     numStars: '4',
+      //     title: "Filly's Taco",
+      //     place: 'City Square Mall',
+      //     imageURL: 'fillys-taco.png',
+      //   },
+      //   {
+      //     numStars: '5',
+      //     title: 'The Fruit Stop',
+      //     place: 'NEX Mall',
+      //     imageURL: 'the-fruit-stop.png',
+      //   },
+      //   {
+      //     numStars: '5',
+      //     title: 'JP Bakery',
+      //     place: 'Jurong Point',
+      //     imageURL: 'jp-bakery.png',
+      //   },
+      // ],
+      businesses: []
     }
   },
+  async mounted() {
+    await this.fetchAndUpdateData();
+  },
+  methods: {
+    async fetchAndUpdateData() {
+      try {
+        const querySnapShot = await getDocs(collection(db, 'Account Details'));
+        querySnapShot.forEach((doc) => {
+          const data = doc.data();
+          if (data.store && data.address /*&& data.image*/) {
+            this.businesses.push({
+              numStars: '4',
+              title: data.store,
+              place: data.address,
+              imageURL: 'bakers-dozen.png' /* data.image */,
+            })
+          }
+        })
+      } catch (error) {
+        // const errorCode = error.code
+        const errorMessage = error.message;
+        alert(errorMessage);
+      }
+    },
+  }
 }
 </script>
 
--- src/views/Landing/LandingPage.vue
+++ src/views/Landing/LandingPage.vue
@@ -9,19 +9,75 @@
     <div class="choose-wrapper">
       <h1>Choose from your favourite stores</h1>
       <router-link to="/catalogue" class="show-all-button">
-        <v-btn
-          size="small"
-          rounded="xl"
-          variant="outlined"
-          color="rgb(29, 41, 57)"
-          dark
-        >
+        <v-btn size="small" rounded="xl" variant="outlined" color="rgb(29, 41, 57)" dark>
           Show All
         </v-btn>
       </router-link>
 
       <div class="StoreCards-wrapper">
-        <StoreCard
+        <v-slide-group v-model="selectedBusinessIndex">
+          <v-slide-group-item v-for="business in businesses" :key="business.title">
+            <StoreCard class="storecard" :numStars="business.numStars" :title="business.title" :place="business.place"
+              :imageURL="business.imageURL" />
+          </v-slide-group-item>
+        </v-slide-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+import StoreCard from '@/components/StoreCard'
+import { db } from '@/firebaseConfig'
+import { getDocs, collection } from 'firebase/firestore'
+
+export default {
+  name: 'LandingPage',
+  components: {
+    NavBar,
+    StoreCard,
+  },
+  data() {
+    return {
+      businesses: [],
+      selectedBusinessIndex: 0 // Initialize the selected business index
+    }
+  },
+  async mounted() {
+    await this.fetchAndUpdateData();
+  },
+  methods: {
+    async fetchAndUpdateData() {
+      try {
+        const querySnapShot = await getDocs(collection(db, 'Account Details'));
+        querySnapShot.forEach((doc) => {
+          const data = doc.data();
+          if (data.store && data.address /*&& data.image*/) {
+            this.businesses.push({
+              numStars: '4',
+              title: data.store,
+              place: data.address,
+              imageURL: 'bakers-dozen.png' /* data.image */,
+            })
+          }
+        })
+      } catch (error) {
+        // const errorCode = error.code
+        const errorMessage = error.message;
+        alert(errorMessage);
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+@import './style.css';
+</style>
+
+
+<!-- <StoreCard
           :numStars="4"
           title="The Baker's Dozen"
           place="Raffles Place"
@@ -47,26 +103,4 @@
             variant="outlined"
           >
             <v-icon icon="mdi-chevron-right"></v-icon>
-          </v-btn>
-        </v-row>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import NavBar from '@/components/NavBar'
-import StoreCard from '@/components/StoreCard'
-
-export default {
-  name: 'LandingPage',
-  components: {
-    NavBar,
-    StoreCard,
-  },
-}
-</script>
-
-<style scoped>
-@import './style.css';
-</style>
+          </v-btn> -->
