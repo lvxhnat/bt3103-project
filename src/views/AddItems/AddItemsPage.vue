1da0cf116623597cb0e067932313978a7a5b87cf
tinghwei <e0968057@u.nus.edu>
2024-04-23 11:24:48 +0800

    Removed some files that were wrongly added

--- src/views/AddItems/AddItemsPage.vue
+++ src/views/AddItems/AddItemsPage.vue
@@ -1,46 +1,43 @@
 <template>
-    <div>
-        <h1>Store: {{ store }}</h1>
-        <v-btn @click="() => TogglePopup('buttonTrigger')">Add Items</v-btn>
-        <AddItems v-if="trigger.buttonTrigger" 
-        :TogglePopup="() => TogglePopup('buttonTrigger')"/>
-        <DisplayItems/>
-    </div>
+  <div>
+    <h1>Store: {{ store }}</h1>
+    <v-btn @click="() => TogglePopup('buttonTrigger')">Add Items</v-btn>
+    <AddItems
+      v-if="trigger.buttonTrigger"
+      :TogglePopup="() => TogglePopup('buttonTrigger')"
+    />
+    <DisplayItems />
+  </div>
 </template>
 
 <script>
-import DisplayItems from "../../components/AddItems/DisplayItems.vue";
-import { ref,} from 'vue';
-import AddItems from "../../components/AddItems/AddItems.vue";
-
-
+import DisplayItems from '../../components/AddItems/DisplayItems.vue'
+import { ref } from 'vue'
+import AddItems from '../../components/AddItems/AddItems.vue'
 
 export default {
-    name: "AddItemsPage",
-    data() {
-        return {
-            store: "",
-        }
-    },  
-    setup() {
-        const trigger = ref({
-            buttonTrigger: false,
-        })
+  name: 'AddItemsPage',
+  data() {
+    return {
+      store: '',
+    }
+  },
+  setup() {
+    const trigger = ref({
+      buttonTrigger: false,
+    })
 
-        const TogglePopup = (tri) => {
-            trigger.value[tri] = !trigger.value[tri]
-        }
+    const TogglePopup = (tri) => {
+      trigger.value[tri] = !trigger.value[tri]
+    }
 
-        return { trigger, TogglePopup }
-    },
-    components:{
-        DisplayItems,
-        AddItems,
-    },
+    return { trigger, TogglePopup }
+  },
+  components: {
+    DisplayItems,
+    AddItems,
+  },
 }
-
 </script>
 
-<style>
-
-</style>
+<style></style>
