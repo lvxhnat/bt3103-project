11d03f2ecadc19b6d396f708567cad849d679934
kkaiyu <127333335+kkaiyu@users.noreply.github.com>
2024-04-26 00:08:12 +0800

    resolved add to cart button

--- src/components/ItemCard/ItemCard.vue
+++ src/components/ItemCard/ItemCard.vue
@@ -17,8 +17,7 @@
 <script>
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { db } from '/src/firebaseConfig.js'
-import { doc, setDoc, getDoc } from 'firebase/firestore'
-import { doc, setDoc, query, collection, where, getDocs } from 'firebase/firestore'
+import { doc, setDoc, query, collection, where, getDocs, getDoc } from 'firebase/firestore'
 
 export default {
   name: 'ItemCard',
@@ -46,25 +45,16 @@ export default {
         if (user) {
           this.useremail = user.email
           const itemRef = doc(db, this.useremail, this.name + ', ' + this.store)
-          
+
           const storeRef = doc(db, this.store, this.name);
           const storeDoc = await getDoc(storeRef);
           const availableQuantity = storeDoc.data().quantity;
 
-          if (availableQuantity >= 1) {
-              await setDoc(itemRef, {
-              store: this.store,
-              name: this.name,
-              price: this.price,
-              quantity: 1,
-              image: this.imageURL
-          });
-            
           const businessQuery = query(
               collection(db, 'businesses'),
               where('email', '==', this.useremail)
-            )
-            
+          )
+
           const businessSnapshot = await getDocs(businessQuery)
           if (businessSnapshot.empty) {
             await setDoc(itemRef, {
@@ -74,15 +64,25 @@ export default {
             quantity: 1,
             image: this.imageURL,
             })
+
+            if (availableQuantity >= 1) {
+              await setDoc(itemRef, {
+              store: this.store,
+              name: this.name,
+              price: this.price,
+              quantity: 1,
+              image: this.imageURL
+            });
+          
             alert(this.name + ' has been added to your cart!')
-            }
-            else {
+
+            } else {
               alert("Item is currently not available!")
             }
+          
           } else {
             alert('Unable to add item on a business account!')
           }
-          
         } else {
           alert('Please log in to add item to your cart!')
           this.$router.push({ path: '/login/user' })
